234fbb83d77bf2cc1bf9fc548793e5c36721faca
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-19 15:18:14 +0300

    remove duplicate

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Good Luck 💪🏾
+function removeDuplicates(array) {
+  return Array.from(new Set(array));
+}
+
+const names = ["hassan","aisha","aisha"];
+const uniquenames = removeDuplicates(names);
+console.log(uniquenames);
